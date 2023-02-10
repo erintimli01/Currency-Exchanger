995b7a4bd08d6a010c3ef119a913ec453b154667
Erin Timlin <erintimlin@gmail.com>
2023-02-10 12:49:45 -0500

    add currency class with static method

--- src/currency.js
+++ src/currency.js
@@ -1,3 +1,19 @@
 export default class Currency {
-  
+  static exchangeRate(rate) {
+    return new Promise(function (resolve, reject) {
+    let request = new XMLHttpRequest();
+    const url = `https://v6.exchangerate-api.com/v6/d01a325dff7c2eaf0535f8a5/latest/USD`;
+    request.addEventListener("loadend", function () {
+      const response = JSON.parse(this.responseText);
+      if (this.status === 200) {
+        resolve(response);
+      } else {
+        reject([this, response]);
+      } 
+    });
+
+    request.open("GET", url, true);
+    request.send();
+    });
+  }
 }
