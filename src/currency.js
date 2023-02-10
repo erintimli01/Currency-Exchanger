39f565dd06644ab4d7c7b7a765d4b92627cebba2
Erin Timlin <erintimlin@gmail.com>
2023-02-10 15:01:20 -0500

    change if statement, WIP

--- src/currency.js
+++ src/currency.js
@@ -1,17 +1,21 @@
 export default class Currency {
-  static exchangeRate(rate) {
+  static exchangeRate(exchangeRate, usd, code) {
     return new Promise(function (resolve, reject) {
     let request = new XMLHttpRequest();
-    const url = `https://v6.exchangerate-api.com/v6/d01a325dff7c2eaf0535f8a5/latest/USD`;
+    const url = `https://v6.exchangerate-api.com/v6${process.env.API_KEY}//latest/USD${code}/${usd}`;
     request.addEventListener("loadend", function () {
       const response = JSON.parse(this.responseText);
-      if (this.status === 200) {
+      if(response.error === false) {
         resolve(response);
       } else {
         reject([this, response]);
-      } 
+      }
+      // if (this.status === 200) {
+      //   resolve(response);
+      // } else {
+      //   reject([this, response]);
+      // } 
     });
-
     request.open("GET", url, true);
     request.send();
     });
