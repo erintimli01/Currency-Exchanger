e51d98c87f34fb49073aa3e54c85ef8cb9b34c9c
Erin Timlin <erintimlin@gmail.com>
2023-02-10 11:42:13 -0500

    add initial files

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'output.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devServer: {
-    contentBase: './dist'
-  },
   devtool: 'eval-source-map',
+  devServer: {               
+    contentBase: './dist'    
+  },
   plugins: [
     new ESLintPlugin(),
-    new CleanWebpackPlugin({
-      verbose: true
-    }),
+    new Dotenv(),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'JavaScript Template',
+      title: 'currency-exchanger',
       template: './src/index.html',
       inject: 'body'
-    })
+    }),
   ],
   module: {
     rules: [
