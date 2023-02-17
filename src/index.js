a8b59df5ce0e2f72f5510f3d4e14bd5afd928a4d
Erin Timlin <erintimlin@gmail.com>
2023-02-17 15:30:54 -0500

    remove commented out code

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ function printError(error) {
     const errorMessage = `There was an error in accessing data: ${err[3]}`;
     document.querySelector("#showResponse").innerText = errorMessage;
   }
-  document.querySelector("#showResponse").classList.add("error"); // Add a CSS class to highlight the error
+  document.querySelector("#showResponse").classList.add("error");
 }
 
 function printElements(currencyConversion) {
@@ -32,11 +32,11 @@ function printElements(currencyConversion) {
   const newCode = currencyConversion[0]["conversion_rates"][currencyCode];
   if(currencyConversion[0]["conversion_rates"][currencyCode] === undefined) {
     document.querySelector("#showResponse").innerText = `There was an error accessing data!`;
-    document.querySelector("#showResponse").classList.add("error"); // Add the "error" class
+    document.querySelector("#showResponse").classList.add("error");
   } else {
     document.querySelector("#showResponse").innerText = `$${usDollars} USD = ${
       newCode * usDollars} ${currencyCode}`;
-    document.querySelector("#showResponse").classList.remove("error"); // Remove the "error" class
+    document.querySelector("#showResponse").classList.remove("error");
   }
 }
 
